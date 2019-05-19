5c18f6734b07ef9b892b7380f4821d0c282ea63e
Tyeth Gundry <tyethgundry@googlemail.com>
2019-05-19 12:51:45 +0100

    Built first failing test, add projects to solution

--- Job/RegEx.Video.JobRunner.Tests/ConsoleTest.cs
+++ Job/RegEx.Video.JobRunner.Tests/ConsoleTest.cs
@@ -1,13 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Reflection;
 namespace RegEx.Video.JobRunner.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ConsoleTest
     {
         [TestMethod]
         public void TestMethod1()
         {
+            var a = Assembly.Load("Regex.Video.JobRunner.ConsoleHost");
+            
         }
     }
 }
