b08fe9cb039cbe2771c4c7b3b3ff6d3154e2492b
Tyeth Gundry <tyethgundry@googlemail.com>
2019-05-19 12:33:32 +0100

    Initial Scaffolding:dotnet classlib, console+mstest, reactredux

---
+++ DataModels/RegEx.Video.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RegEx.Video.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ Job/RegEx.Video.JobRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegEx.Video.JobRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
